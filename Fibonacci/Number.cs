c7db128f1cddde718929882ec9462e965b5b666c
shumilija <shumilija@ro.ru>
2024-08-22 20:52:33 +0700

    [+] Модель числа Фибоначчи.

---
+++ Fibonacci/Number.cs
@@ -0,0 +1,31 @@
+﻿namespace Fibonacci
+{
+	/// <summary>
+	/// Число Фибоначчи.
+	/// </summary>
+	internal struct Number
+	{
+		private int _previous;
+		private int _current;
+
+		public Number()
+		{
+			_previous = -1;
+			_current = 1;
+		}
+
+		/// <summary>
+		/// Значение числа Фибоначчи.
+		/// </summary>
+		internal int Value => _current;
+
+		/// <summary>
+		/// Следующее число Фибоначчи.
+		/// </summary>
+		internal Number Next => new Number
+		{
+			_previous = _current,
+			_current = _previous + _current,
+		};
+	}
+}
