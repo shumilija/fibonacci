8a45a633933f49898ec8071c9c2118d4ebdcecd9
shumilija <shumilija@ro.ru>
2024-08-22 20:57:29 +0700

    [+] Последовательность чисел Фибоначчи.

---
+++ Fibonacci/Sequence.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+
+namespace Fibonacci
+{
+	/// <summary>
+	/// Последовательность чисел Фибоначчи.
+	/// </summary>
+	internal class Sequence : IEnumerator<int>, IEnumerable<int>
+	{
+		private readonly int _count;
+		private int _index;
+
+		private Number _current;
+
+		internal Sequence(int count)
+		{
+			_count = count;
+
+			Reset();
+		}
+
+		/// <summary>
+		/// Значение текущего элемента в последовательности.
+		/// </summary>
+		public int Current => _current.Value;
+
+		/// <summary>
+		/// Значение текущего элемента в последовательности.
+		/// </summary>
+		object IEnumerator.Current => Current;
+
+		public void Dispose() { }
+
+		/// <summary>
+		/// Сгенерировать следующий элемент последовательности.
+		/// </summary>
+		/// <returns>
+		///		<c>false</c>, если достигнут конец последовательности,
+		///		<c>true</c>, если последовательность продолжается.
+		/// </returns>
+		public bool MoveNext()
+		{
+			if (_index + 1 == _count)
+			{
+				return false;
+			}
+
+			_current = _current.Next;
+
+			_index++;
+
+			return true;
+		}
+
+		/// <summary>
+		/// Вернуться к началу последовательности.
+		/// </summary>
+		public void Reset()
+		{
+			_index = -1;
+			_current = new Number();
+		}
+
+		/// <summary>
+		/// Получить итератор последовательности Фибоначчи.
+		/// </summary>
+		/// <returns>Итератор последовательности Фибоначчи.</returns>
+		public IEnumerator<int> GetEnumerator() => this;
+
+		/// <summary>
+		/// Получить итератор последовательности Фибоначчи.
+		/// </summary>
+		/// <returns>Итератор последовательности Фибоначчи.</returns>
+		IEnumerator IEnumerable.GetEnumerator() => this;
+	}
+}
---
+++ Fibonacci/Test.cs
@@ -0,0 +1,33 @@
+﻿namespace Fibonacci
+{
+	/// <summary>
+	/// Тестирование расчета чисел Фибоначчи.
+	/// </summary>
+	public class Test
+	{
+		private static readonly int[] Sample =
+		{
+			0, 1, 1, 2, 3, 5, 8, 13, 21,
+		};
+
+		/// <summary>
+		/// Протестировать последовательность, реализованную через IEnumerable/IEnumerator.
+		/// </summary>
+		/// <param name="count">Кол-во элементов в последовательности.</param>
+		[Theory]
+		[InlineData(7)]
+		[InlineData(9)]
+		internal void TestSequence(int count)
+		{
+			var sequence = new Sequence(count);
+
+			int i = 0;
+			foreach (var item in sequence)
+			{
+				Assert.Equal(Sample[i], item);
+
+				i++;
+			}
+		}
+	}
+}
